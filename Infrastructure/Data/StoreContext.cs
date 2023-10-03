643b7fd99ef875f76b5087710d9b10e2b951f60b
H.Bdair <H.Bdair@YAZAN-ABDULLAH>
2023-10-03 10:04:15 +0300

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace E_Commerce.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products{ get; set; }
+    }
+}
